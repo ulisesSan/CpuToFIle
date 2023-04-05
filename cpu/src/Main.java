21140d99510dc4b90ae34f2608e6cf140a8d46a5
Ulises Santiago Caceres Licona <37051136+ulisesSan@users.noreply.github.com>
2023-04-05 16:45:17 -0600

    Added some coments

--- cpu/src/Main.java
+++ cpu/src/Main.java
@@ -1,19 +1,21 @@
 import java.io.*;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws IOException {
+
         tasksCpu fileWrite = new tasksCpu();
         String cmd = "cpu";
+        //Accedemos a la camtidad de nucleos para una futura caracteristica
         int cores = Runtime.getRuntime().availableProcessors();
         String output;
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         try{
+            //Se ejecuta el comando
             Process job = Runtime.getRuntime().exec(cmd);
             BufferedReader input = new BufferedReader(new InputStreamReader(job.getInputStream()));
             System.out.println(cores);
+            //leemos y almacenamos la informacion
             while ((output= input.readLine()) != null) {
+                //Agregamos los datos a nuestro metodo
                 fileWrite.writeFile(output, cores);
             }
         }catch(Exception e){
--- cpu/src/tasksCpu.java
+++ cpu/src/tasksCpu.java
@@ -3,32 +3,31 @@ import java.util.Arrays;
 
 public class tasksCpu {
     public static void writeFile(String output, int cores) throws IOException {
+        //Se crea la ruta para el archivo
+        //se crea el archivo en la ruta especificada
         String archivo = System.getProperty("user.home") + "/cpuInfo.txt";
         File file = new File(archivo.replace("\\", "/"));
+        //Se crea objeto encargado de escribir en el archivo el archivo
         FileWriter Writer = new FileWriter(archivo, true);
+        //Se crea un buffer
         BufferedWriter bWriter = new BufferedWriter(Writer);
+        //en caso de que exista el archivo solo escribe en el
         if(file.exists() && !file.isDirectory()){
             try{
-                PrintWriter print = new PrintWriter(bWriter);
+                //escribe en el archivo
                 bWriter.write(output +"\r\n");
-                bWriter.newLine();
             }catch(Exception e){
                 System.out.println(e);
             }finally {
                 bWriter.close();
             }
         }else{
+            //en caso de que no exista el archivo lo crea y escribe las primeras lineas en el
             file.createNewFile();
             System.out.println("File created succesfully");
             try{
-
-                PrintWriter print = new PrintWriter(bWriter);
-                print.write(output);
-                for(int i = 0; i <= cores; i++){
-
-                    //System.out.println(output[]);
-                }
-
+                //Escribe las primeras lineas
+                bWriter.write(output);
             }catch(Exception e){
                 System.out.println(e);
             }finally{
